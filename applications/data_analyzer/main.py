0ab2e4d145c44471d709dcb4e1421ccb3f257aae
Jesse Martin <pokemonjm@gmail.com>
2024-03-07 11:10:08 -0500

    Update main.py

--- applications/data_analyzer/main.py
+++ applications/data_analyzer/main.py
@@ -9,7 +9,17 @@ def index():
 
 @app.route('/run-analysis')
 def run_analysis():
-    return "test"
+    tweets = requests.get("https://fxes.onrender.com/api/tweets").json()
+    unsent_tweets = []
+    for tweet in tweets:
+        if tweet['email_notified'] == False:
+            unsent_tweets.append(tweet)
+    str = ''
+    for tweet in unsent_tweets:
+        user = requests.get(f"https://fxes.onrender.com/api/users/{tweet['associated_user']}").json()
+        username = user['username']
+        str += username + ": " + tweet['text'] + " \n"
+    return str
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
