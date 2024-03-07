38f2e76eb3f99dbc19b9e85fbd26811d0a2e857b
Jesse Martin <pokemonjm@gmail.com>
2024-03-07 09:11:25 -0500

    queue listen

--- applications/data_analyzer/main.py
+++ applications/data_analyzer/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import requests
+import redis
+from rq import Worker, Queue, Connection
 
 app = Flask(__name__)
 
@@ -7,5 +9,12 @@ app = Flask(__name__)
 def index():
     return 'This is the route for the data analyzer!'
 
+listen = ['default']
+redis_url = "redis://red-cnksjben7f5s73b1v9q0:6379"
+conn = redis.from_url(redis_url)
+
 if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(list(map(Queue,listen)))
+        worker.work()
     app.run(debug=True, host="0.0.0.0")
